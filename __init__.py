6312a61f0d8d5cdc16c3a62a926b5034737cbdd9
Andy Gill <andygillku@gmail.com>
2024-12-25 21:46:35 -0600

    Porting Load and Save 3D Mesh

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
-NODE_CLASS_MAPPINGS = {}
+import os
+import sys
 
-NODE_DISPLAY_NAME_MAPPINGS = {}
+import folder_paths as comfy_paths
+
+
+ROOT_PATH = os.path.join(
+    comfy_paths.get_folder_paths("custom_nodes")[0], "ComfyUI-3D-Pack-Lite"
+)
+sys.path.append(ROOT_PATH)
+
+from . import nodes
+
+
+NODE_CLASS_MAPPINGS = nodes.NODE_CLASS_MAPPINGS
+
+NODE_DISPLAY_NAME_MAPPINGS = nodes.NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
--- nodes.py
+++ nodes.py
@@ -1,1 +1,113 @@
-# Empty
+# This is a port of parts of https://github.com/MrForExample/ComfyUI-3D-Pack for OSX.
+# The original ComfyUI-3D-Pack was not compatible with OSX.
+#
+# The orignal code was released, MIT, Copyright (c) 2024 MrForExample, and the link to
+# the LICENSE is here: https://github.com/MrForExample/ComfyUI-3D-Pack/blob/main/LICENSE
+#
+#
+# Only selected nodes have been ported. The footprint is much smaller than the original.
+
+import os
+import torch
+
+import folder_paths as comfy_paths
+from os.path import isfile, join, exists, dirname
+from datetime import datetime
+
+from .mesh_processer.mesh import Mesh
+
+from .shared_utils.log_utils import cstr
+from .shared_utils.common_utils import parse_save_filename
+
+SUPPORTED_3D_EXTENSIONS = (
+    ".obj",
+    ".ply",
+    ".glb",
+)
+
+
+
+class Load_3D_Mesh:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "mesh_file_path": ("STRING", {"default": '', "multiline": False}),
+                "resize":  ("BOOLEAN", {"default": False},),
+                "renormal":  ("BOOLEAN", {"default": True},),
+                "retex":  ("BOOLEAN", {"default": False},),
+                "optimizable": ("BOOLEAN", {"default": False},),
+                "clean": ("BOOLEAN", {"default": False},),
+                "resize_bound": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1000.0, "step": 0.001}),
+            },
+        }
+
+    RETURN_TYPES = (
+        "MESH",
+    )
+    RETURN_NAMES = (
+        "mesh",
+    )
+    FUNCTION = "load_mesh"
+    CATEGORY = "Comfy3D/Import|Export"
+    
+    def load_mesh(self, mesh_file_path, resize, renormal, retex, optimizable, clean, resize_bound):
+        mesh = None
+        
+        if not os.path.isabs(mesh_file_path):
+            mesh_file_path = os.path.join(comfy_paths.input_directory, mesh_file_path)
+        
+        if os.path.exists(mesh_file_path):
+            folder, filename = os.path.split(mesh_file_path)
+            if filename.lower().endswith(SUPPORTED_3D_EXTENSIONS):
+                with torch.inference_mode(not optimizable):
+                    mesh = Mesh.load(mesh_file_path, resize, renormal, retex, clean, resize_bound)
+            else:
+                cstr(f"[{self.__class__.__name__}] File name {filename} does not end with supported 3D file extensions: {SUPPORTED_3D_EXTENSIONS}").error.print()
+        else:        
+            cstr(f"[{self.__class__.__name__}] File {mesh_file_path} does not exist").error.print()
+        return (mesh, )
+    
+
+
+class Save_3D_Mesh:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "mesh": ("MESH",),
+                "save_path": ("STRING", {"default": 'Mesh_%Y-%m-%d-%M-%S-%f.glb', "multiline": False}),
+            },
+        }
+
+    OUTPUT_NODE = True
+    RETURN_TYPES = (
+        "STRING",
+    )
+    RETURN_NAMES = (
+        "save_path",
+    )
+    FUNCTION = "save_mesh"
+    CATEGORY = "Comfy3D/Import|Export"
+    
+    def save_mesh(self, mesh, save_path):
+        save_path = parse_save_filename(save_path, comfy_paths.output_directory, SUPPORTED_3D_EXTENSIONS, self.__class__.__name__)
+        
+        if save_path is not None:
+            mesh.write(save_path)
+
+        return (save_path, )
+    
+
+NODE_CLASS_MAPPINGS = {
+    "Load_3D_Mesh": Load_3D_Mesh,
+    "Save_3D_Mesh": Save_3D_Mesh,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Load_3D_Mesh": "Load_3D_Mesh",
+    "Save_3D_Mesh": "Save_3D_Mesh",
+}
